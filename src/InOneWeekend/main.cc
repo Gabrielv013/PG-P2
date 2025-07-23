ecc6614faf9743183f517166ee3cbe8b2d1798f4
Gabrielv013 <gabrielv@estudante.ufscar.br>
2025-07-23 16:20:52 -0300

    Main atualizada para criação da cena modificada

--- src/InOneWeekend/main.cc
+++ src/InOneWeekend/main.cc
@@ -24,53 +24,95 @@ int main() {
     auto ground_material = make_shared<lambertian>(color(0.5, 0.5, 0.5));
     world.add(make_shared<sphere>(point3(0,-1000,0), 1000, ground_material));
 
-    for (int a = -11; a < 11; a++) {
-        for (int b = -11; b < 11; b++) {
+    for (int a = -10; a < 10; a++) {
+        for (int b = -10; b < 10; b++) {
+            if(random_double() < 0.7) // diminuir quantidade de esferas
+                continue;
+            
             auto choose_mat = random_double();
-            point3 center(a + 0.9*random_double(), 0.2, b + 0.9*random_double());
+            point3 center(a + 1.2*random_double(), 0.2, b + 1.2*random_double());
 
-            if ((center - point3(4, 0.2, 0)).length() > 0.9) {
-                shared_ptr<material> sphere_material;
+            shared_ptr<material> sphere_material;
 
+            if (choose_mat < 0.8) {
+                // diffuse
+                auto albedo = color::random() * color::random();
+                sphere_material = make_shared<lambertian>(albedo);
+                world.add(make_shared<sphere>(center, 0.2, sphere_material));
+            } else if (choose_mat < 0.95) {
+                 // metal
+                auto albedo = color::random(0.5, 1);
+                auto fuzz = random_double(0, 0.5);
+                sphere_material = make_shared<metal>(albedo, fuzz);
+                world.add(make_shared<sphere>(center, 0.2, sphere_material));
+            } else {
+                // glass
+                sphere_material = make_shared<dielectric>(1.5);
+                world.add(make_shared<sphere>(center, 0.2, sphere_material));
+            }
+
+        }
+    }
+    
+    // matriz de posição de desenho de esferas na vertical
+    // 1 significa que será desenhada uma esfera na posição
+    std::vector<std::vector<bool>> m = {
+        //  B            C           C
+        {1,1,1,0,  0, 0,1,1,1, 0, 0,1,1,1},
+        {1,0,0,1,  0, 1,0,0,0, 0, 1,0,0,0},
+        {1,1,1,0,  0, 1,0,0,0, 0, 1,0,0,0},
+        {1,0,0,1,  0, 1,0,0,0, 0, 1,0,0,0},
+        {1,1,1,0,  0, 0,1,1,1, 0, 0,1,1,1},
+    };
+    
+    double raio = 0.3;
+    shared_ptr<material> material;
+    
+    int altura = m.size();
+    int largura = m[0].size();
+    
+    for(int i = 0; i < altura; i++) {
+        for(int j = 0; j < largura; j++) {
+            if(m[i][j]) {
+                auto choose_mat = random_double();
+                
+                double x = j * (raio * 2.5);
+                double y = i * (raio * 2.5) + raio + 1.0;
+                double z = -5;
+                
                 if (choose_mat < 0.8) {
                     // diffuse
                     auto albedo = color::random() * color::random();
-                    sphere_material = make_shared<lambertian>(albedo);
-                    world.add(make_shared<sphere>(center, 0.2, sphere_material));
+                    material = make_shared<lambertian>(albedo);
                 } else if (choose_mat < 0.95) {
                     // metal
                     auto albedo = color::random(0.5, 1);
                     auto fuzz = random_double(0, 0.5);
-                    sphere_material = make_shared<metal>(albedo, fuzz);
-                    world.add(make_shared<sphere>(center, 0.2, sphere_material));
+                    material = make_shared<metal>(albedo, fuzz);
                 } else {
                     // glass
-                    sphere_material = make_shared<dielectric>(1.5);
-                    world.add(make_shared<sphere>(center, 0.2, sphere_material));
+                    material = make_shared<dielectric>(1.5);
                 }
+                
+                world.add(make_shared<sphere>(point3(x,y,z), raio, material));
             }
         }
     }
-
-    auto material1 = make_shared<dielectric>(1.5);
-    world.add(make_shared<sphere>(point3(0, 1, 0), 1.0, material1));
-
-    auto material2 = make_shared<lambertian>(color(0.4, 0.2, 0.1));
-    world.add(make_shared<sphere>(point3(-4, 1, 0), 1.0, material2));
-
-    auto material3 = make_shared<metal>(color(0.7, 0.6, 0.5), 0.0);
-    world.add(make_shared<sphere>(point3(4, 1, 0), 1.0, material3));
 
     camera cam;
 
     cam.aspect_ratio      = 16.0 / 9.0;
     cam.image_width       = 1200;
-    cam.samples_per_pixel = 10;
+    cam.samples_per_pixel = 20;
     cam.max_depth         = 20;
+    
+    double espacamento = raio * 2.5;
+    double yCentro = (altura-1) * espacamento / 2.0 + 1.0; // 1.0 é a altura adicionada nas esferas para que elas flutuem
+    double xCentro = (largura-1) * espacamento / 2.0;
 
     cam.vfov     = 20;
-    cam.lookfrom = point3(13,2,3);
-    cam.lookat   = point3(0,0,0);
+    cam.lookfrom = point3(xCentro,yCentro,15);
+    cam.lookat   = point3(xCentro,yCentro,0);
     cam.vup      = vec3(0,1,0);
 
     cam.defocus_angle = 0.6;
